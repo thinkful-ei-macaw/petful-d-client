603152a7dd72be7992502b571ead9499873b8ec1
Desmond Wareham <desmond.wareham@gmail.com>
2020-07-14 17:21:11 -0400

    deploying

---
+++ build/precache-manifest.7f91a8c946e0a318fac237bd3ed66d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87adfbc2631a16260d8a7f75cdf828dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea588fd74a9cc086edc",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6107a254752408609bf4",
+    "url": "/static/js/2.17051432.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.17051432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea588fd74a9cc086edc",
+    "url": "/static/js/main.d5717f8c.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 export default {
-  API_ENDPOINT:'https://bref-chocolatine-21548.herokuapp.com/' || "http://localhost:7000",
+  API_ENDPOINT:'https://bref-chocolatine-21548.herokuapp.com' || "http://localhost:7000",
 };
 
 // export default {
