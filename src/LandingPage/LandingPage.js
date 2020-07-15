ecf50cc29c6c3e449c439904f1651dbc1b5f145f
Desmond Wareham <desmond.wareham@gmail.com>
2020-07-14 21:24:17 -0400

    updated landing

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
     return (
         <div className='landing-page'>
-            <p>Looking for a pet to adopt? Get started by pressing the button below!</p>
-            <p>Sign up to get in line to adopt a cat or a dog.</p>
+            <p>Looking for a pet to adopt? Get started by pressing the "Click Me" below!</p>
+            <p>You can Adoption a cat, a dog, or both! Adoption is based on FIFO order so you'll have to wait in line to adopt. Once your name is up select the pet that you want to adopt!</p>
             <Link to="/adopt">Click Me</Link>
         </div>
     )
