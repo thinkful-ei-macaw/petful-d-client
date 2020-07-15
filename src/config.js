f0df6a95406e35cdf85b0b78036e0a092527ccca
Desmond Wareham <desmond.wareham@gmail.com>
2020-07-14 20:32:31 -0400

    deploying

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-// export default {
-//   API_ENDPOINT:'https://bref-chocolatine-21548.herokuapp.com' || "http://localhost:7000",
-// };
-
 export default {
-  API_ENDPOINT:"http://localhost:7000",
+  API_ENDPOINT:'https://bref-chocolatine-21548.herokuapp.com' || "http://localhost:7000",
 };
+
+// export default {
+//   API_ENDPOINT:"http://localhost:7000",
+// };
 
